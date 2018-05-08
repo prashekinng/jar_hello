888a0c6d96e7a63e3b127305edb01fbe112f95a4
Your Name <you@example.com>
2018-05-08 14:45:13 -0400

    added src

---
+++ src/main/java/jar_hello/App.java
@@ -0,0 +1,13 @@
+package jar_hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
